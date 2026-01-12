5eb8ab3ba4c7365ae30ee4cf358f98a67ad3ad58
bngcminh <buingocminh2k4@gmail.com>
2026-01-12 16:51:17 +0700

    delete link head

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,35 +1,50 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const music = [
-    {
-       name: 'A Stranger I Remain',
-       singer: 'Platinum Mix',
-       path: '../public/music/Metal Gear Rising - A Stranger I Remain (Original + Platinum Mix Combination) + Lyrics.mp3', 
-       img: '../public/img/Tachyon.jpg'
-    },
-    {
-       name: 'A Soul Can"t Be Cut',
-       singer: 'Platinum Mix',
-       path: '../public/music/Metal Gear Rising- Revengeance Soundtrack - 09. A Soul Can"t Be Cut (Platinum Mix).mp3', 
-       img: '../public/img/Tachyon.jpg'
-    },
-    {
-       name: 'Collective Consciousness',
-       singer: 'Platinum Mix',
-       path: '../public/music/Metal Gear Rising- Revengeance Soundtrack - 10. Collective Consciousness (Maniac Agenda Mix).mp3', 
-       img: '../public/img/Tachyon.jpg'
-    },
-    {
-       name: 'It Has to Be This Way',
-       singer: 'Platinum Mix',
-       path: '../public/music/Metal Gear Rising- Revengeance Soundtrack - 11. It Has to Be This Way (Platinum Mix).mp3', 
-       img: '../public/img/Tachyon.jpg'
-    },
-    {
-       name: 'The War Still Rages Within',
-       singer: 'Platinum Mix',
-       path: '../public/music/Metal Gear Rising- Revengeance Soundtrack - 12. The War Still Rages Within.mp3', 
-       img: '../public/img/Tachyon.jpg'
-    },
-]
+const player = $(".player");
+const cd = $(".cd");
+const heading = $("header h2");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const playBtn = $(".btn-toggle-play");
+const progress = $("#progress");
+const prevBtn = $(".btn-prev");
+const nextBtn = $(".btn-next");
+const randomBtn = $(".btn-random");
+const repeatBtn = $(".btn-repeat");
+const playlist = $(".playlist");
+
+const music = {
+    song: [
+        {
+            name: 'A Stranger I Remain',
+            singer: 'Platinum Mix',
+            path: '../public/music/Metal Gear Rising - A Stranger I Remain (Original + Platinum Mix Combination) + Lyrics.mp3', 
+            img: '../public/img/Tachyon.jpg'
+        },
+        {
+            name: 'A Soul Can"t Be Cut',
+            singer: 'Platinum Mix',
+            path: '../public/music/Metal Gear Rising- Revengeance Soundtrack - 09. A Soul Can"t Be Cut (Platinum Mix).mp3', 
+            img: '../public/img/Tachyon.jpg'
+        },
+        {
+            name: 'Collective Consciousness',
+            singer: 'Platinum Mix',
+            path: '../public/music/Metal Gear Rising- Revengeance Soundtrack - 10. Collective Consciousness (Maniac Agenda Mix).mp3', 
+            img: '../public/img/Tachyon.jpg'
+        },
+        {
+            name: 'It Has to Be This Way',
+            singer: 'Platinum Mix',
+            path: '../public/music/Metal Gear Rising- Revengeance Soundtrack - 11. It Has to Be This Way (Platinum Mix).mp3', 
+            img: '../public/img/Tachyon.jpg'
+        },
+        {
+            name: 'The War Still Rages Within',
+            singer: 'Platinum Mix',
+            path: '../public/music/Metal Gear Rising- Revengeance Soundtrack - 12. The War Still Rages Within.mp3', 
+            img: '../public/img/Tachyon.jpg'
+        },
+    ],
+}
